30317de9bb3e8f2ce963335743dc95dfb65eec29
naveenjha031 <naveenjha344@gmail.com>
2025-03-05 11:18:06 +0530

    [Naveen] Add : Calculate wage Acc to time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,5 @@
 //uc 1
-const IS_PRESENT = 0;
+const IS_PRESENT = 1;
 let empCheck = Math.floor((Math.random()* 10) % 2); 
 
 if (empCheck === IS_PRESENT) {
@@ -7,3 +7,27 @@ if (empCheck === IS_PRESENT) {
 } else {
     console.log("Employee is Absent");
 }
+
+//uc 2
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+
+let empHours = 0;
+
+switch (empCheck) {
+    case PART_TIME:
+        empHours = PART_TIME_HOURS;
+        break;
+    case FULL_TIME:
+        empHours = FULL_TIME_HOURS;
+        break;
+    default:
+        empHours = 0; 
+}
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
